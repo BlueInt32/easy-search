1c3000de2eb499957d559017a36e4c1159e26fa1
BlueInt32 <simon.budin@gmail.com>
2026-05-28 05:52:27 +0200

    Fix launching with title and logo

--- launch-easy-search.sh
+++ launch-easy-search.sh
@@ -5,6 +5,6 @@ WINDOW_ID=$(wmctrl -l | grep " easy-search$" | awk '{print $1}' | head -1)
 if [ -n "$WINDOW_ID" ]; then
     wmctrl -ia "$WINDOW_ID"
 else
-    kstart5 --maximize -- /home/simon/.cargo/bin/alacritty --title "easy-search" \
+    kstart5 --maximize -- /home/simon/.cargo/bin/alacritty --title "easy-search" --class easy-search,easy-search --config-file ~/.config/easy-search-alacritty.toml \
         -e bash --login -c 'tmux new-session "/home/simon/dev/easy-search/target/release/easy-search; dir=\$(cat /tmp/easy-search_lastdir 2>/dev/null); rm -f /tmp/easy-search_lastdir; [ -n \"\$dir\" ] && cd \"\$dir\"; exec bash --login -i"'
 fi
